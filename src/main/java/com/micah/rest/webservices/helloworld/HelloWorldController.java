9b97ce81b771d6141c09bb49ed8882078ee5363e
Micah <ismiki977@gmail.com>
2024-07-26 15:49:26 +0300

    feat:Add i18n and swagger UI(documentation)

--- src/main/java/com/micah/rest/webservices/helloworld/HelloWorldController.java
+++ src/main/java/com/micah/rest/webservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.micah.rest.webservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    private MessageSource msgSrc;
+    public HelloWorldController(MessageSource msgSrc){
+        this.msgSrc = msgSrc;
+    }
+
+
     @GetMapping(path = "hello-world")
     public String helloWorld() {
         return "Hello World!";
@@ -20,5 +30,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    //i18n
+    @GetMapping(path = "/hello-world-i18n")
+    public String helloWorldInternationalization() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return msgSrc.getMessage("good.morning.message", null, "Default Message", locale) ;
     }
 }
